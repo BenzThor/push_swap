fd326e3e0048d5d2477b7a0539ed493a8392bee9
Thorben Benz <tbenz@c2r4p9.42vienna.com>
2023-11-09 13:23:42 +0100

    save

--- sources/determine_target.c
+++ sources/determine_target.c
@@ -6,7 +6,7 @@
 /*   By: tbenz <tbenz@student.42vienna.com>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/11/09 12:26:49 by tbenz             #+#    #+#             */
-/*   Updated: 2023/11/09 13:02:46 by tbenz            ###   ########.fr       */
+/*   Updated: 2023/11/09 13:22:36 by tbenz            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -37,19 +37,53 @@ void	ft_determine_closest_b(t_sort *sort, t_stack *elem)
 void	ft_determine_targets_b(t_sort *sort)
 {
 	t_stack	*temp_a;
-	t_stack	*temp_b;
-	t_stack	*target;
-	t_stack	*lowest_b;
 
-	lowest_b = ft_stack_lowest(sort->st_b);
 	temp_a = sort->st_a;;
 	while (sort->st_a)
 	{
-		if (sort->st_a->ind > lowest_b)
-			sort->st_a->trgt = sort->st_b;
+		if (sort->st_a->ind < ft_stack_lowest(sort->st_b))
+			sort->st_a->trgt = ft_stack_highest(sort->st_b);
 		else
 			ft_determine_closest(sort, sort->st_a);
 		sort->st_a = sort->st_a->ptr;
 	}
 	sort->st_a = temp_a;
+}
+
+void	ft_determine_closest_a(t_sort *sort, t_stack *elem)
+{
+	t_stack	*temp_a;
+
+	temp_a = sort->st_a;
+	while (sort->st_a)
+	{
+		if (!elem->trgt)
+		{
+			if (sort->st_a->ind > elem->ind)
+				elem->trgt = temp_a;
+		}
+		else
+		{
+			if (sort->st_a > elem->trgt->ind)
+				elem->trgt = sort->st_a;
+		}
+		sort->st_a = sort->st_a->ptr;
+	}
+	sort->st_a = temp_a;
+}
+
+void	ft_determine_targets_a(t_sort *sort)
+{
+	t_stack	*temp_b;
+
+	temp_b = sort->st_b;
+	while (sort->st_b)
+	{
+		if (sort->st_b->ind > ft_stack_highest(sort->st_a))
+			sort->st_b->trgt = ft_stack_lowest(sort->st_a);
+		else
+			ft_determine_closest(sort, sort->st_b);
+		sort->st_b = sort->st_b->ptr;
+	}
+	sort->st_b = temp_b;
 }
