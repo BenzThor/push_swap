ad57daf2685a9c5b2881f71c429df8dc82a55805
Thorben Benz <tbenz@c2r4p9.42vienna.com>
2023-11-10 18:16:51 +0100

    save

--- sources/push_swap.c
+++ sources/push_swap.c
@@ -6,7 +6,7 @@
 /*   By: tbenz <tbenz@student.42vienna.com>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/11/05 10:41:38 by tbenz             #+#    #+#             */
-/*   Updated: 2023/11/10 18:09:58 by tbenz            ###   ########.fr       */
+/*   Updated: 2023/11/10 18:16:33 by tbenz            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,8 +15,6 @@
 int	main(int argc, char **argv)
 {
 	t_sort	sort;
-	t_stack	*temp1;
-	t_stack	*temp2;
 
 	ft_init_sort(&sort);
 	ft_check_arguments(argc, argv, &sort);
